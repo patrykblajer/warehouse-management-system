650e7dd46b2c22641698545a681724dd4fbaa94b
Patryk <patrykblajer@gmail.com>
2022-04-14 22:55:19 +0200

    initial commit

---
+++ src/test/java/dev/patbla/warehousemanagementsystem/WarehouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patbla.warehousemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
