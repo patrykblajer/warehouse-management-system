46adb5150325ba2b48108b954a121a97a82d8469
Patryk <patrykblajer@gmail.com>
2022-04-19 23:50:08 +0200

    add new frontend design, new topbar and menu

--- src/frontend/src/components/Header/Header.js
+++ src/frontend/src/components/Header/Header.js
@@ -1,30 +1,24 @@
-import styles from './Header.module.scss'
+import style from './Header.module.scss'
 import React, { Component } from 'react'
 function Header(props) {
 	return (
-		<div>
-			<div className={`${styles.topBar}`}>
-				<div className={`${styles.weatherWidgetContainer}`}>Warehouse Management System</div>
-				<div className={`${styles.themeColorSwitcher}`}></div>
-				<div className={`${styles.accountContainer}`}>
-					<div className={`${styles.accountInfo}`}>
-						<b>John Doe (ID: 1)</b>
-						<p>administrator systemu</p>
-					</div>
-					<div className={`${styles.accountAvatar}`}></div>
-
-					<div className={`${styles.accountActions}`}>
-						<a href='/settings' title='wyloguj'>
-							<button type='button' className='btn btn-secondary btn-sm'>
-								<i className={`fas fa-cog ${styles.profileSettings}`}></i>
-							</button>
-						</a>
-						<a href='/logout' title='wyloguj'>
-							<button type='button' className='btn btn-secondary btn-sm'>
-								<i className={`fas fa-sign-out-alt ${styles.profileSettings}`}></i>
-							</button>
-						</a>
-					</div>
+		<div className={style.topBar}>
+			<div className={style.accountContainer}>
+				<div className={style.accountInfoItem}>
+					<i class='fa-solid fa-briefcase'></i>Administrator systemu
+				</div>
+				<div className={style.accountInfoItem}>
+					<i class='fa-solid fa-user'></i>John Doe (ID: 1)
+				</div>
+				<div className={style.accountInfoItem}>
+					<a href='/settings' title='wyloguj'>
+						<i className={`fas fa-cog`}></i>Ustawienia konta
+					</a>
+				</div>
+				<div className={style.accountInfoItem}>
+					<a href='/logout' title='wyloguj'>
+						<i className={`fas fa-sign-out-alt`}></i>Wyloguj
+					</a>
 				</div>
 			</div>
 		</div>
--- src/frontend/src/components/Menu/Menu.js
+++ src/frontend/src/components/Menu/Menu.js
@@ -1,49 +1,51 @@
-import styles from './Menu.module.scss'
+import style from './Menu.module.scss'
 import { Link, NavLink } from 'react-router-dom'
 import React from 'react'
 
 function Menu() {
 	return (
-		<aside className={`${styles.menuContainer}`}>
+		<aside className={`${style.menuContainer}`}>
 			<nav>
-				<ul>
-					<li>
-						<NavLink exact to='/'>
+				<ul className={style.menuList}>
+					<li className={style.menuListItem}>
+						<NavLink exact to='/' className={data => (data.isActive ? `${style.menuListItemActive}` : '')}>
 							<i className='fas fa-home'></i>Strona główna
 						</NavLink>
 					</li>
-					<li>
-						<NavLink to='/products'>
+					<li className={style.menuListItem}>
+						<NavLink
+							exact
+							to='/products'
+							className={navData => (navData.isActive ? `${style.menuListItemActive}` : '')}>
 							<i className='fas fa-box'></i>Asortyment
 						</NavLink>
 					</li>
-					<li>
+					<li className={style.menuListItem}>
 						<NavLink to={`/#`}>
 							<i className='fas fa-shipping-fast'></i>Wydania magazynowe
 						</NavLink>
 					</li>
-
-					<li>
+					<li className={style.menuListItem}>
 						<NavLink to={`/#`}>
 							<i className='fas fa-truck-loading'></i>Przyjęcia zewnętrzne
 						</NavLink>
 					</li>
-					<li>
+					<li className={style.menuListItem}>
 						<NavLink to={`/#`}>
 							<i className='fas fa-people-carry'></i>Zlecenia przeniesienia
 						</NavLink>
 					</li>
-					<li>
+					<li className={style.menuListItem}>
 						<NavLink to={`/#`}>
 							<i className='fas fa-clipboard'></i>Inwentaryzacja
 						</NavLink>
 					</li>
-					<li>
+					<li className={style.menuListItem}>
 						<NavLink to={`/#`}>
 							<i className='fas fa-hands-helping'></i>Kontrahenci
 						</NavLink>
 					</li>
-					<li>
+					<li className={`${style.menuListItem} ${style.menuListItemLast}`}>
 						<NavLink to={`/#`}>
 							<i className='fas fa-wrench'></i>Konfiguracja systemu
 						</NavLink>
