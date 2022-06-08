c9d46a64ccc46f5d3ad1c69899f12c3a4851ef82
Patryk <patrykblajer@gmail.com>
2022-06-08 23:44:50 +0200

    add user and role entities + spring security with jwt

---
+++ src/main/java/dev/patbla/warehousemanagementsystem/user/AppUserService.java
@@ -0,0 +1,22 @@
+package dev.patbla.warehousemanagementsystem.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) appUserRepository.findAppUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user with such username!"));
+    }
+}
---
+++ src/main/java/dev/patbla/warehousemanagementsystem/user/role/RoleRepository.java
@@ -0,0 +1,6 @@
+package dev.patbla.warehousemanagementsystem.user.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<UserRole, Long> {
+}
